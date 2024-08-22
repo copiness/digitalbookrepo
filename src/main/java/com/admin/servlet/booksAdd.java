0f1e8234b035a9e16c33d5b45346e352abf7ade2
91809 <91809@LAPTOP-1E0T04S2>
2024-08-23 00:48:18 +0530

    add book

--- src/main/java/com/admin/servlet/booksAdd.java
+++ src/main/java/com/admin/servlet/booksAdd.java
@@ -4,18 +4,21 @@ import java.io.IOException;
 
 
 
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part; 
+import javax.servlet.http.Part;
+
+import com.company.dao.BooksDAOimpl; 
 
 @WebServlet("/add_books")
-public class booksAdd extends HttpServlet {
+public class booksAdd<BooksDAOImpl> extends HttpServlet {
 	
 	
-	protected void dopost(HttpServletRequest req, HttpServletResponse resp) throws ServletException ,IOException{
+	protected void dopost(HttpServletRequest req, HttpServletResponse resp, Object DBConnect) throws ServletException ,IOException{
 	 try {
 		  String bookName = req.getParameter("bname");
 		  String author = req.getParameter("author");
@@ -26,7 +29,22 @@ public class booksAdd extends HttpServlet {
 		  String fileName = req.getParameter("fileName");
 
 		  BookDtls b = new BookDtls(bookName, author , price , categories , status , fileName , "admin");
-		  System.out.println(b);
+		  BooksDAOImpl dao=new BooksDAOImpl(DBConnect.getconn());
+		  
+		  boolean f = dao.addBooks(b);
+		  
+		  HttpSession session=req.getSession();
+		  
+		  if(f) {
+			  session.setAttribute("succMsg","Book Add Sucessfully");
+			  resp.sendRedirect("admin/add_book.jsp");
+		  }
+		  else {
+			  session.setAttribute("faileMsg","Something wrong on server");
+			  resp.sendRedirect("admin/add_book.jsp");
+			  
+		  }
+		
 		  
 
 	  
--- src/main/java/com/entity/BookDtls.java
+++ src/main/java/com/entity/BookDtls.java
@@ -2,4 +2,44 @@ package com.entity;
 
 public class BookDtls {
 
+	public String getBookName1() {
+		
+		return null;
+	}
+	public String getAuthor() {
+		
+		return null;
+	}
+
+	public String getPrice() {
+		
+		return null;
+	}
+	public String getBookCategor() {
+		
+		return null;
+	}
+
+	
+	
+
+	public String getStatus() {
+		
+		return null;
+	}
+
+	public String getPhotoName() {
+		
+		return null;
+	}
+
+	public String getEmail() {
+		
+		return null;
+	}
+	
 }
+	
+
+
+
